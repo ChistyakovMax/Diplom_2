1da1da5d8ee75f6031f3e2029ab4676571e7d756
Max Chistyakov <chistyakovmaxim2000@gmail.com>
2022-11-17 20:12:31 +0300

    test data deleting dragged to @After method

--- src/test/java/site/stellarburgers/order/GetOrdersTest.java
+++ src/test/java/site/stellarburgers/order/GetOrdersTest.java
@@ -1,19 +1,28 @@
 package site.stellarburgers.order;
 
 import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
 @DisplayName("Получение заказов")
 public class GetOrdersTest extends OrderBaseTest {
 
+    String accessToken;
+
+    @After
+    public void deleteUser(){
+        if (accessToken != null) {
+            userClient.delete(accessToken);
+        }
+    }
+
     @Test
     @DisplayName("Получение заказов как авторизованный пользователь")
     public void getOrdersAuthorizedReturnSuccess() {
-        String accessToken = OrderBaseTest.createUserAndOrder(order, orderClient, user, userClient);
+        accessToken = OrderBaseTest.createUserAndOrder(order, orderClient, user, userClient);
         boolean isSuccess = orderClient.getOrdersWithAuth(accessToken);
         Assert.assertTrue(isSuccess);
-        userClient.delete(accessToken);
     }
 
     @Test
